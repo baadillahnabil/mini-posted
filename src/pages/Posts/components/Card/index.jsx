2c845192ab7ab0b4e9b280b2fb2fc7b33778a436
baadillahnabil <nabilzero27@gmail.com>
2019-10-17 13:50:55 +0700

    remove props spread and specify onclick

--- src/pages/Posts/components/Card/index.jsx
+++ src/pages/Posts/components/Card/index.jsx
@@ -1,12 +1,15 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
 import React from 'react'
 import PropTypes from 'prop-types'
 import withStyles from 'react-jss'
 
 import styles from './styles'
 
-const Card = ({ classes, title, author, body, ...props }) => {
+const Card = ({ classes, title, author, body, onClick }) => {
   return (
-    <div className={classes.root} {...props}>
+    <div className={classes.root} onClick={onClick}>
       <p className={classes.title}>{title}</p>
       {body !== '' && <p className={classes.body}>{body}</p>}
       <p className={classes.author}>&quot;{author}&quot;</p>
@@ -18,10 +21,12 @@ Card.propTypes = {
   classes: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  body: PropTypes.string
+  body: PropTypes.string,
+  onClick: PropTypes.func
 }
 Card.defaultProps = {
-  body: ''
+  body: '',
+  onClick: () => {}
 }
 
 export default withStyles(styles)(Card)
