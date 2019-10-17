1479202f8b6a205912af48e65c3d81b8fe2de726
baadillahnabil <nabilzero27@gmail.com>
2019-10-17 14:09:52 +0700

    add ability to customize style

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import withStyles from 'react-jss'
+import clsx from 'clsx'
 
 import styles from './styles'
 
-const Button = ({ classes, children, ...props }) => {
+const Button = ({ classes, children, customClasses, ...props }) => {
   return (
     <div className={classes.root}>
-      <button type="button" className={classes.button} {...props}>
+      <button
+        type="button"
+        className={clsx(classes.button, customClasses.button)}
+        {...props}
+      >
         {children}
       </button>
     </div>
@@ -16,8 +21,13 @@ const Button = ({ classes, children, ...props }) => {
 
 Button.propTypes = {
   classes: PropTypes.object.isRequired,
-  children: PropTypes.any.isRequired
+  children: PropTypes.any.isRequired,
+  customClasses: PropTypes.objectOf(PropTypes.string)
 }
-Button.defaultProps = {}
+Button.defaultProps = {
+  customClasses: {
+    button: ''
+  }
+}
 
 export default withStyles(styles)(Button)
