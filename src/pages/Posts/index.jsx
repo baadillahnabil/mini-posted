024027b6cdb354b15423003d35e7e01c8377eb4f
baadillahnabil <nabilzero27@gmail.com>
2019-10-17 12:56:29 +0700

    add clickable post.

--- src/pages/Posts/index.jsx
+++ src/pages/Posts/index.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import withStyles from 'react-jss'
+import { withRouter } from 'react-router-dom'
 
 import Card from './components/Card'
 
 import db from '../../db/data.json'
 import styles from './styles'
 
-const Posts = ({ classes }) => {
+const Posts = ({ classes, history }) => {
   return (
     <div className={classes.root}>
       <div className={classes.cardsWrapper}>
         {db.map(data => (
-          <Card key={data.id} title={data.title} author={data.author} />
+          <Card
+            key={data.id}
+            title={data.title}
+            author={data.author}
+            clickable
+            onClick={() => history.push(`/post/${data.id}`)}
+          />
         ))}
       </div>
     </div>
@@ -20,7 +27,8 @@ const Posts = ({ classes }) => {
 }
 
 Posts.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  history: PropTypes.object.isRequired
 }
 
-export default withStyles(styles)(Posts)
+export default withRouter(withStyles(styles)(Posts))
